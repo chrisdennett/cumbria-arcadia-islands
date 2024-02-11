e78a9c9164608740a9c0875de6738be6c0951271
chrisdennett <chrisdennett@gmail.com>
2024-02-11 08:14:48 +0000

    Added a basic grid

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ const canvas = document.getElementById('artCanvas');
 
 canvas.width = 400;
 canvas.height = 400;
+
+const ctx = canvas.getContext("2d");
+
+const cellSize = 40;
+const cols = Math.floor(canvas.width / cellSize);
+const rows = Math.floor(canvas.height / cellSize);
+
+for(let r = 0; r<rows; r++){
+    for(let c = 0; c<rows; c++){
+        const cellX = r*cellSize;
+        const cellY = c*cellSize;
+        ctx.strokeRect(cellX,cellY,cellSize, cellSize);
+    }
+}
