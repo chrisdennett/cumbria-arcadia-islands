f956ce1bcf4bde988ae797002c27572a1619bc1a
chrisdennett <chrisdennett@gmail.com>
2024-02-11 08:05:59 +0000

    basic setup

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-// Import stylesheets
-import './style.css';
-
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-const canvas = document.createElement('canvas');
-appDiv.appendChild(canvas);
+const canvas = document.getElementById('artCanvas');
 
 canvas.width = 400;
 canvas.height = 400;
