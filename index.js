b3fce6bba6fd5e493931a4c3551fda8995625070
chrisdennett <chrisdennett@gmail.com>
2024-02-16 22:28:08 +0000

    added tile stacking

--- index.js
+++ index.js
@@ -22,21 +22,32 @@ const sunkenOffset = 6;
 const altRowVOffset = 8;
 
 const tileMap = [];
-tileMap.push([0,1,1,1,1,1,1,1,0,0]);
-tileMap.push([0,1,1,1,2,2,1,1,0,0]);
-tileMap.push([0,0,1,1,2,2,1,1,0,0]);
-tileMap.push([0,1,1,1,2,2,1,1,0,0]);
-tileMap.push([1,0,1,1,2,2,1,1,1,0]);
-tileMap.push([1,1,1,1,2,2,1,1,0,0]);
-tileMap.push([0,0,1,1,1,1,1,1,0,0]);
-tileMap.push([0,0,0,1,1,1,1,1,0,0]);
+tileMap.push([0, 1,   1,1,2,2,1,1,0,0]);
+
+tileMap.push([0, 1,   1,1,2,2,1,1,0,0]);
+tileMap.push([0, 0,   1,1,2,2,1,1,0,0]);
+tileMap.push([0, 1,   1,1,2,2,1,1,0,0]);
+tileMap.push([1, 0,   1,1,2,2,1,1,1,0]);
+tileMap.push([1, 1,   1,1,2,2,1,1,0,0]);
+tileMap.push([0, 0,   1,1,1,1,1,1,0,0]);
+tileMap.push([0, 0,   0,1,1,1,1,1,0,0]);
+tileMap.push([0, 0, "1-1-1",1,1,1,1,1,0,0]);
 
 let tileY = 0;
 
 for(let i=0; i<tileMap.length; i++){
     const row = tileMap[i];
     for(let j=0; j<row.length; j++){
-        const cellType = row[j];
+        let cellType = row[j];
+
+        // split values like 1.1.1
+        const str = cellType.toString();
+        let layers = [];
+        if(str.indexOf("-") > 0){
+            let layerStrings = str.split("-");
+            cellType = parseInt(layerStrings.shift());
+            layers = layerStrings.map(str => parseInt(str));
+        }
       
         let tileX = cellSize * j;
 
@@ -44,16 +55,29 @@ for(let i=0; i<tileMap.length; i++){
             tileX += halfOffset;
         }
 
-        if(cellType === 1){
-            ctx.drawImage(landTile, tileX, tileY);
-        }
-        if(cellType === 2){ 
-            ctx.drawImage(waterTile, tileX, tileY+sunkenOffset);
+        drawTile(cellType, tileX, tileY);
+
+        for(let i=0; i<layers.length; i++){
+            const type = layers[i];
+            console.log(type);
+            const layerOffset = (i+1) * halfOffset;
+            const layerY = tileY - layerOffset;
+            console.log(layerY);
+            drawTile(type, tileX, layerY);
         }
     }
 
     tileY += altRowVOffset;
 }
 
-ctx.drawImage(landTile, halfOffset+cellSize*2, altRowVOffset);
+function drawTile(type, x, y){
+    if(type === 1){
+        ctx.drawImage(landTile, x, y);
+    }
+    if(type === 2){ 
+        ctx.drawImage(waterTile, x, y+sunkenOffset);
+    }
+}
+
+// ctx.drawImage(landTile, halfOffset+cellSize*2, altRowVOffset);
 
